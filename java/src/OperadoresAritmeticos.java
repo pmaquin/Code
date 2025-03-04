58207740fa7bb785b1d2deb39171ad229cf6457e
pmaquin <pauloctaquino@gmail.com>
2025-03-04 00:40:42 -0300

    add java

---
+++ java/src/OperadoresAritmeticos.java
@@ -0,0 +1,34 @@
+public class OperadoresAritmeticos {
+    
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 5;
+        
+        int soma = x + y;
+        System.out.println("Soma: " + soma);
+
+        int subtracao = x - y;
+        System.out.println("Subtracao: " + subtracao);
+
+        int multiplicacao = x * y;
+        System.out.println("Multiplicação: " + multiplicacao);
+
+        int divisao = x / y;
+        System.out.println("Divisão: " + divisao);
+
+        int modulo = x % y;
+        System.out.println("Modulo: " + modulo);
+
+        int incremento = 10;
+        int decremento = 10;
+
+        incremento++;
+        System.out.println("Incremento: " + incremento);
+
+        decremento--;
+        System.out.println("Decremento: " + decremento);
+
+
+
+    }
+}
