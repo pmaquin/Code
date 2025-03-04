f8a0e14dd5a6469eb8457a32f5bb6f31aa3cb50b
pmaquin <pauloctaquino@gmail.com>
2025-03-04 01:25:17 -0300

    att

--- java/src/TabelaVerdade.java
+++ java/src/TabelaVerdade.java
@@ -2,19 +2,19 @@ public class TabelaVerdade {
     public static void main(String [] args) {
         System.out.println("Tabela Verdade para o Operador AND (&&):");
         System.out.println("A\tB\tA && B");
-        System.out.println(true + "\t" + true + "\t" + (true && true));
-        System.out.println(true + "\t" + false + "\t" + (true && false));
-        System.out.println(false + "\t" + true + "\t" + (false && true));
-        System.out.println(false + "\t" + false + "\t" + (false && false));
+        System.out.println(true + "\t" + true + "\t" + (true));
+        System.out.println(true + "\t" + false + "\t" + (false));
+        System.out.println(false + "\t" + true + "\t" + (false));
+        System.out.println(false + "\t" + false + "\t" + (false));
 
         System.out.println();
         System.out.println("Tabela Verdade para o Operador OR (||)");
 
         System.out.println("A\tB\tA || B");
-        System.out.println(true + "\t" + true + "\t" + (true || true));
-        System.out.println(true + "\t" + false + "\t" + (true || false));
-        System.out.println(false + "\t" + true + "\t" + (false || true));
-        System.out.println(false + "\t" + false + "\t" + (false || false));
+        System.out.println(true + "\t" + true + "\t" + (true));
+        System.out.println(true + "\t" + false + "\t" + (true));
+        System.out.println(false + "\t" + true + "\t" + (true));
+        System.out.println(false + "\t" + false + "\t" + (false));
     
 
     }
